7dc72092484da42f0f20fb97871a9df77ed338f2
Романенко Р.В. <r.romanenko@kol.tech.lan>
2026-03-20 18:06:31 +0300

    update UX UI

--- frontend/react/src/pages/Login/Login.jsx
+++ frontend/react/src/pages/Login/Login.jsx
@@ -1,9 +1,35 @@
 import { styled } from "styled-components"
+import { SectionInput, Button } from "../../components"
 
 const LoginContainer = ({ className }) => {
     return (
-        <div className={className}>Login</div>
+        <div className={className}>
+            <h2>Login</h2>
+            <form action="POST" className="login--form">
+                <SectionInput inputType="email" inputName="email" inputPlaceholder="" labelTitle="Электронная почта" />
+                <SectionInput inputType="password" inputName="password" inputPlaceholder="" labelTitle="Пароль" />
+
+                <Button typeButton="submit">Войти</Button>
+            </form>
+        </div>
     )
 }
 
-export const Login = styled(LoginContainer)``
+export const Login = styled(LoginContainer)`
+
+    display: flex;
+    flex-direction: column;
+    
+    width: 30%;
+    padding: 10px 20px;
+    margin: auto;
+
+    box-shadow: 4px 3px 10px black;
+
+    & .login--form {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        padding: 10px;
+    }
+`
